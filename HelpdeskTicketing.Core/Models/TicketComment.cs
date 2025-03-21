3657dad3628deeb5e700aa96ab871b99c056faba
Sid Prabaharan <137012018+sidprabaharan@users.noreply.github.com>
2025-03-21 12:29:41 -0400

    Create TicketComment.cs

---
+++ HelpdeskTicketing.Core/Models/TicketComment.cs
@@ -0,0 +1,19 @@
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public bool IsInternal { get; set; } = false;
+
+        public int TicketId { get; set; }
+        public virtual Ticket Ticket { get; set; }
+
+        public string AuthorId { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+    }
+}
